4199d9f43c3f819344d6a851c6d3f8eae0c97402
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2021-04-30 20:23:52 +0700

    router link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Navbar from './component/Navbar'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App">
-      Belajar yuk
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { CgMenuRight } from "react-icons/cg";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
 const Navbar = () => {
+  const [sidebar, setSidebar] = useState(false)
+  const showSidebar = () => setSidebar(!sidebar)
   return (
     <>
-      
+    {/* Hamburger Menu */}
+      <div className="navbar">
+        <Link to="#" className="menu-bars">
+          <CgMenuRight onClick={showSidebar} />
+        </Link>
+      </div>
+    {/* Sidebar Menu */}
+      <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+        <ul className="nav-menu-items">
+          <li className="navbar-toggle">
+            <Link to="#" className="Menu-bars">
+              <AiOutlineCloseCircle />
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </>
   )
 }
